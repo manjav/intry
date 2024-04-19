64878f2f8a94809f943dd107ad597a62fcba24ac
Mansour <mansurjavadi@gmail.com>
2024-04-19 17:24:11 +0300

    Add template package project

---
+++ example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Intry Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Intry Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
---
+++ lib/src/intry_numeric.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+enum IntryState {
+  normal,
+  editting,
+}
+
+class NumericIntry extends StatefulWidget {
+
+  const NumericIntry({
+    super.key,
+  });
+
+  @override
+  State<NumericIntry> createState() => _NumericIntryState();
+}
+
+class _NumericIntryState extends State<NumericIntry> {
+
+  @override
+  Widget build(BuildContext context) {
+  }
+  }
