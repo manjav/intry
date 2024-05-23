01759d31a28b20a040bb4db9f22939812b60f37e
Mansour <mansurjavadi@gmail.com>
2024-05-23 12:44:20 +0300

    cleanups -  update test codes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _numberValue = 0;
+  String _stringValue = "Hello";
 
   @override
   Widget build(BuildContext context) {
@@ -43,16 +43,18 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             NumericIntry(
+              inputType: IntryInputType.text,
               min: 0,
               max: 100,
               divisions: 5,
               postfix: "%",
-              value: _numberValue,
-              onChanged: (double value) => setState(() => _numberValue = value),
+              value: _stringValue,
+              onTextChanged: (String value) =>
+                  setState(() => _stringValue = value),
             ),
             const SizedBox(width: 30),
             Text(
-              'Min: 0\nMax: 100\nDivision: 5\nPostfix: "%"\nValue: $_numberValue',
+              'Min: 0\nMax: 100\nDivision: 5\nPostfix: "%"\nValue: $_stringValue',
               style: Theme.of(context).textTheme.titleSmall,
             ),
           ],
