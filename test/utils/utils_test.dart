c1ca99ec206d0a8c31e527725d12ab6b0ba64a60
Mansour <mansurjavadi@gmail.com>
2024-05-30 15:58:32 +0300

    testing -  add divide unit test

--- test/utils/utils_test.dart
+++ test/utils/utils_test.dart
@@ -43,4 +43,41 @@ void main() {
     });
   });
 
+  group('Utils.divide', () {
+    test(
+        'should divide the given value by the number of divisions and round it to the nearest integer',
+        () {
+      expect(Utils.divide(12.5, divisions: 10), equals(10));
+      expect(Utils.divide(17, divisions: 10), equals(20));
+    });
+    test(
+        'should return the given value if the number of divisions is less than 1',
+        () {
+      expect(Utils.divide(12.5, divisions: 0), equals(12.5));
+      expect(Utils.divide(17, divisions: -1), equals(17));
+    });
+    test('should handle fractional values', () {
+      expect(Utils.divide(12.75, divisions: 2), equals(12));
+      expect(Utils.divide(17.25, divisions: 5), equals(15));
+    });
+    test('should handle negative values', () {
+      expect(Utils.divide(-12.5, divisions: 10), equals(-10));
+      expect(Utils.divide(-17, divisions: 10), equals(-20));
+    });
+    test('should handle zero values', () {
+      expect(Utils.divide(0, divisions: 10), equals(0));
+    });
+  });
+
+  test('Replace Persian / Arabic digits with Latin digits', () {
+    expect(
+      '۰۱۲۳۴۵۶۷۸۹'.toLatin(),
+      equals('0123456789'),
+    );
+    expect('۰۴٥۶۷۸'.toLatin(), equals('045678'));
+    expect(''.toLatin(), equals(''));
+    expect('abcdefghijklmnopqrstuvwxyz'.toLatin(),
+        equals('abcdefghijklmnopqrstuvwxyz'));
+    expect('numbers۱۳۵'.toLatin(), equals('numbers135'));
+  });
 }
